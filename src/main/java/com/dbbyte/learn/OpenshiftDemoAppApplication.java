120da5805d01e1b4907685413e7a16e962f4ea11
adriancuria <curia.gheorghe@yahoo.com>
2018-01-06 17:08:56 +0200

    1th

---
+++ src/main/java/com/dbbyte/learn/OpenshiftDemoAppApplication.java
@@ -0,0 +1,12 @@
+package com.dbbyte.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenshiftDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenshiftDemoAppApplication.class, args);
+	}
+}
